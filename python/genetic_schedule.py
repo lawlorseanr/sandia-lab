0ffde5bea7b8ccffb2f25d2ec67f3794bd546065
Sean Lawlor <blue.dusk0520@fastmail.com>
2015-06-30 14:06:04 -0800

    genetic schedule toy

---
+++ python/genetic_schedule.py
@@ -0,0 +1,10 @@
+"""Toy genetic algorithm for a tiny job-shop schedule."""
+import random
+
+def fitness(order):
+    return -sum(abs(a - b) for a, b in zip(order, sorted(order)))
+
+def mutate(order):
+    i, j = random.sample(range(len(order)), 2)
+    order[i], order[j] = order[j], order[i]
+    return order
