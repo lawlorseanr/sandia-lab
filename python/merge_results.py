8bf5a0e5d7b24311829fc189040347eb69f26f3e
Sean Lawlor <blue.dusk0520@fastmail.com>
2015-06-30 14:48:18 -0800

    merge csv outputs

---
+++ python/merge_results.py
@@ -0,0 +1,9 @@
+"""Merge CSV sweep outputs."""
+import csv, glob
+
+def merge(pattern, out):
+    rows = []
+    for path in glob.glob(pattern):
+        with open(path) as f:
+            rows.extend(list(csv.DictReader(f)))
+    return rows
